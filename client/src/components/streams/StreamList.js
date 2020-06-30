d093d27f08364de9c9c70e8f5f93ee0dd028feb1
devmobin <mobinheydari114@gmail.com>
2020-07-01 00:48:18 +0430

    create stream button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { fetchStreams } from '../../actions'
 
 class StreamList extends Component {
@@ -14,7 +15,7 @@ class StreamList extends Component {
         <i className="large middle aligned icon camera" />
         <div className="content">
           {stream.title}
-          <div className="description">{stream.description}</div>  
+          <div className="description">{stream.description}</div>
         </div>
       </div>
     ))
@@ -31,21 +32,33 @@ class StreamList extends Component {
     }
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
-        <div className="ui celled list">
-          {this.renderList()}
-        </div>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => ({ 
+const mapStateToProps = (state) => ({
   streams: Object.values(state.streams),
-  currentUserId: state.auth.userId
+  currentUserId: state.auth.userId,
+  isSignedIn: state.auth.isSignedIn,
 })
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList)
