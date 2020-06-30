98c84e3b30b1e89128a1ccb67b0eff64e63161a4
devmobin <mobinheydari114@gmail.com>
2020-07-01 01:41:05 +0430

    connect component to redux

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-function StreamEdit() {
+function StreamEdit(props) {
   return (
     <div>
       Stream Edit
@@ -8,4 +9,8 @@ function StreamEdit() {
   )
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id]
+})
+
+export default connect(mapStateToProps)(StreamEdit)
