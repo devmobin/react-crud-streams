5d0293a981a5840c798bedabc8199a2d86241ba8
devmobin <mobinheydari114@gmail.com>
2020-07-01 01:08:32 +0430

    custom history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from 'react'
+import { Router, Route } from 'react-router-dom'
 import StreamList from './streams/StreamList'
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
 import StreamShow from './streams/StreamShow'
-import Header from './Header';
+import Header from './Header'
+import history from '../history'
 
 function App() {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -19,9 +20,9 @@ function App() {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
