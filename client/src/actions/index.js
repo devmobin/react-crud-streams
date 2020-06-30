2e9696b1987cbbb829d76ca3dfa7c27b5f6e7130
devmobin <mobinheydari114@gmail.com>
2020-07-01 00:32:37 +0430

    save user id with the streams they create

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,8 +14,9 @@ export const SignOut = () => {
   }
 }
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues)
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth
+  const response = await streams.post('/streams', { ...formValues, userId })
 
   dispatch({ type: types.CREATE_STREAM, payload: response.data })
 }
