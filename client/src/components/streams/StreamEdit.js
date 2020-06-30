533e0905db4fa5aa0c67c58bce0e281c31d99962
devmobin <mobinheydari114@gmail.com>
2020-07-01 02:17:28 +0430

    set initial values

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
 
+  onSubmit = (formValues) => {}
+
   render() {
     if (!this.props.stream) {
-      return (
-        <div>Loading...</div>
-      )
+      return <div>Loading...</div>
     }
 
     return (
       <div>
-        Stream Edit
+        <h3>Edit a stream</h3>
+        <StreamForm
+          initialValues={this.props.stream}
+          onSubmit={this.onSubmit}
+        />
       </div>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  stream: state.streams[ownProps.match.params.id]
+  stream: state.streams[ownProps.match.params.id],
 })
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+const mapDispatchToProps = {
+  fetchStream,
+  editStream,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit)
