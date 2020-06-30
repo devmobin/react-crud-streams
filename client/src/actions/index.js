03de59044a48cd418c26f2d8191b758b1c9ee4a1
devmobin <mobinheydari114@gmail.com>
2020-06-30 19:59:48 +0430

    create stream on the server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,10 @@
 import * as types from './types'
+import streams from '../apis/streams'
 
 export const SignIn = (userId) => {
   return {
     type: types.SIGN_IN,
-    payload: userId
+    payload: userId,
   }
 }
 
@@ -11,4 +12,8 @@ export const SignOut = () => {
   return {
     type: types.SIGN_OUT,
   }
+}
+
+export const createStream = (formValues) => async (dispatch) => {
+  const res = await streams.post('/streams', formValues)
 }
