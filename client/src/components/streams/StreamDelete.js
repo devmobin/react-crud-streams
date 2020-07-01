09e39e4c163d4d12bc2b2208680d9b6474df8be1
devmobin <mobinheydari114@gmail.com>
2020-07-01 18:03:44 +0430

    reafctor into calss base component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import history from '../../history'
 import Modal from '../Modal'
 
-function StreamDelete() {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  )
+class StreamDelete extends Component {
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    )  
+  }
 
-  return (
-    <div>
-      Stream Delete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push('/')}
-      />
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        Stream Delete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    )
+  }
 }
 
 export default StreamDelete
